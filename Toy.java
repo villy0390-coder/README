6164fba60d7911cde8575eff54dd464e1c9d4122
villy0390-coder <villy.0390@gmail.com>
2026-05-11 04:55:09 +0000

    day5

---
+++ Toy.java
@@ -0,0 +1,11 @@
+public class Toy {
+    public String name;
+    public String brand;
+    public double price;
+    public int quantity;
+    //psvm - public ststic void main(String[] args) {}
+    public static void main(String[] args) {
+        //sout System.out.println("hello java trainees")
+        System.out.println("hello JAVAwockees");
+    }
+}
