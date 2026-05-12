320443dd64e2f33eddd66d38e1b69ac0fa1959e4
villy0390-coder <villy.0390@gmail.com>
2026-05-12 04:09:44 +0000

    day6

--- Toy.java
+++ Toy.java
@@ -1,11 +1,14 @@
 public class Toy {
-    public String name;
+    public String name = "Tamiya mini 4WD";
     public String brand;
     public double price;
     public int quantity;
     //psvm - public ststic void main(String[] args) {}
     public static void main(String[] args) {
         //sout System.out.println("hello java trainees")
-        System.out.println("hello JAVAwockees");
+        System.out.println("hello Java Trainees");
+        Toy toy1 = new Toy();
+        System.out.println(toy1.name);
     }
+    //file name shoud be the one in public class
 }
